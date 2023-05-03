bbd680749addb7cbfd2350a011458dd747d68019
Maksym Yeroshevych <makserosh95@gmail.com>
2023-05-03 19:29:17 +0300

    +

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Notiflix from 'notiflix';
+
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import axios from "axios";
